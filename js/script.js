96b6a163d28fa26b6190ae1dda1a1abe08a61af0
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-08-13 16:55:00 -0300

    implemento scrollreveal.js--Comienzo con el hero

---
+++ js/script.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+
+sr.reveal('#heroTitle', {
+    duration: 5000,
+    origin: 'right',
+    distance: '-150px'
+});
+
+sr.reveal('#heroContent', {
+    duration: 6500,
+    origin: 'right',
+    distance: '-150px'
+});
+
+sr.reveal('#heroSubtitle', {
+    duration: 5000,
+    origin: 'left',
+    distance: '-150px'
+});
+
+sr.reveal('#heroNumber', {
+    duration: 6000,
+    origin: 'left',
+    distance: '-150px'
+});
+
+sr.reveal('#heroContact', {
+    duration: 7000,
+    origin: 'left',
+    distance: '-150px'
+});
