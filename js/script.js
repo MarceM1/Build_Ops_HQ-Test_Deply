91f0cf73ffdc4e98b156953485a1004ec238a5f6
Marcelo Melogno <marcelomelogno1986@gmail.com>
2023-08-22 02:16:27 -0300

    ScrollReveal.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,23 @@
 window.sr = ScrollReveal();
 
-sr.reveal('#heroTitle', {
+sr.reveal('.toright', {
     duration: 5000,
     origin: 'right',
     distance: '-150px'
 });
-
-sr.reveal('#heroContent', {
-    duration: 6500,
-    origin: 'right',
-    distance: '-150px'
-});
-
-sr.reveal('#heroSubtitle', {
+sr.reveal('.toleft', {
     duration: 5000,
     origin: 'left',
     distance: '-150px'
 });
-
-sr.reveal('#heroNumber', {
-    duration: 6000,
-    origin: 'left',
+sr.reveal('.todown', {
+    duration: 5000,
+    origin: 'bottom',
+    distance: '-150px'
+});
+sr.reveal('.toup', {
+    duration: 5000,
+    origin: 'top',
     distance: '-150px'
 });
 
-sr.reveal('#heroContact', {
-    duration: 7000,
-    origin: 'left',
-    distance: '-150px'
-});
